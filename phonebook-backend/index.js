544b21d9f1682325a735d86a6fe9af7c94322694
wngtk <100271394+wngtk@users.noreply.github.com>
2023-11-22 21:31:44 +0800

    use MongoDB

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors');
+const Person = require('./models/person')
 const app = express();
 
 app.use(cors())
@@ -45,7 +47,9 @@ let persons = [
 ];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons);
+  })
 });
 
 const nextId = () => {
@@ -60,45 +64,72 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  const exists = persons.find((person) => person.name === body.name);
-  if (exists) {
-    return res.status(400).json({
-      error: `name must be unique`,
-    });
-  }
-
-  const person = {
+  const person = new Person({
     id: nextId(),
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    {
-      res.status(404).end();
-    }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
   }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.get("/info", (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`);
+    })
+    .catch(error => next(error))
 });
 
-app.get("/info", (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`);
-});
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length != 3 && process.argv.length != 5) {
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const URL = `mongodb+srv://fnmainrs:${password}@phonebook.3n6hl4o.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(URL)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(res => {
+        console.log(`added ${res.name} ${res.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log(`phonebook:`)
+    Person.find({}).then(res => {
+        res.forEach((person => {
+            console.log(`${person.name} ${person.number}`)
+        }))
+        mongoose.connection.close()
+    })
+}
